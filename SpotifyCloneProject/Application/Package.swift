4f5faf8cda0880b8732ba77a35d7650a9efaf346
Victor Henrique <victorh1705@gmail.com>
2023-04-10 17:57:32 -0300

    refactor: remove comments from package file

--- SpotifyCloneProject/Application/Package.swift
+++ SpotifyCloneProject/Application/Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
   name: "Application",
   products: [
-    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "Models",
       targets: ["Models"]),
@@ -15,8 +14,6 @@ let package = Package(
       targets: ["Utils"]),
   ],
   targets: [
-    // Targets are the basic building blocks of a package, defining a module or a test suite.
-    // Targets can depend on other targets in this package and products from dependencies.
     .target(
       name: "Models",
       dependencies: []),
